d0e18066aac4fa8b650c1ee7d6fd7dc5ae02f6eb
unknown <liai.pasupathi@gmail.com>
2021-10-19 22:39:21 +0530

    pasupathi

---
+++ src/navigations/index.js
@@ -0,0 +1,19 @@
+ import { NavigationContainer } from '@react-navigation/native';
+ import React from 'react';
+ import {
+   Text,
+ } from 'react-native';
+ 
+ 
+ const AppNavication = () => {
+   return (
+     <NavigationContainer>
+        <Text>
+        welcome
+        </Text>
+     </NavigationContainer>
+   );
+ };
+ 
+ export default AppNavication;
+ 
